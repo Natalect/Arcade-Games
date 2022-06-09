b31a3ad40edc7ac22147e366ecd59b934a37ca8a
Natalect <Natalect.org@protonmail.com>
2022-06-08 23:20:14 -0700

    Final setup

---
+++ javascript/main.js
@@ -0,0 +1,51 @@
+function launchAsteroids() {
+  window.location = "./Asteroids/index.html";
+}
+
+function launchBomberman() {
+  window.location = "./Bomberman/index.html";
+}
+
+function launchBrickBreaker() {
+  window.location = "./Brick-Breakerindex.html";
+}
+
+function launchCentipede() {
+  window.location = "./Centipede/index.html";
+}
+
+function launchFlappyBird() {
+  window.location = "./Flappy-Bird/index.html";
+}
+
+function launchFrogger() {
+  window.location = "./Frogger/index.html";
+}
+
+function launchFruitNinja() {
+  window.location = "./Fruit-Ninja/index.html";
+}
+
+function launchMinesweeper() {
+  window.location = "./Minesweeper/index.html";
+}
+
+function launchPacman() {
+  window.location = "./Pacman/index.html";
+}
+
+function launchPong() {
+  window.location = "./Pong/index.html";
+}
+
+function launchSnake() {
+  window.location = "./Snake/index.html";
+}
+
+function launchSpaceInvaders() {
+  window.location = "./Space-Invaders/index.html";
+}
+
+function launchTetris() {
+  window.location = "./Tetris/index.html";
+}
